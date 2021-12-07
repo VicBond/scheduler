9eef7ab84315fdf43c43c2d1af663afd77a6cbf8
Victor Bondarenko <nirasimha@gmail.com>
2021-12-07 16:15:01 -0500

    DayList created

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,1 +1,12 @@
-<ul></ul> 
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList(props){
+  const parsedDayList = props.days.map(day => <DayListItem 
+    key={day.id} {...day} 
+    selected={day.name === props.day} 
+    setDay={props.setDay} />);
+  return(
+    <ul>{parsedDayList}</ul>
+  )
+};
