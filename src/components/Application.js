feb2bfd8a46230e2b6da62260ea349f3c0036076
Victor Bondarenko <nirasimha@gmail.com>
2021-12-09 16:45:48 -0500

    setDay and setDays are combined into useState

--- src/components/Application.js
+++ src/components/Application.js
@@ -45,14 +45,19 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+  });
+  
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     axios
-    .get(`http://localhost:8001/api/days`)
+    .get(`/api/days`)
     .then(response => {
-      setDays([...response.data]);
+      setDays(response.data);
     });
   }, []);
 
@@ -67,8 +72,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList 
-            days={days} 
-            value={day} 
+            days={state.days} 
+            value={state.day} 
             onChange={setDay}
           />
         </nav>
