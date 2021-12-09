c506f977c20b1b66daf2f33c81707f4c014a490c
Victor Bondarenko <nirasimha@gmail.com>
2021-12-08 19:54:27 -0500

    Displaying appointments added to appointment/index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { Fragment } from 'react'
 import "./styles.scss";
 
+// Appointments components
+import Header from "components/Appointment/Header.js";
+import Empty from "components/Appointment/Empty.js";
+import Show from "components/Appointment/Show.js";
+// import Confirm from "components/Appointment/Confirm.js";
+// import Status from "components/Appointment/Status.js";
+// import Error from "components/Appointment/Error.js";
+// import Form from "components/Appointment/Form.js";
 
 export default function Appointment(props) {
-
   return (
-  <article className="appointment"></article>)
+  <article className="appointment">
+    <Header time={props.time} />
+    {props.interview ? 
+    < Show
+     student={props.interview.student}
+     interviewer={props.interview.interviewer}
+     /> : < Empty />}
+  </article>)
   ;
 
 }
