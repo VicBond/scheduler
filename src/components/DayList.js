0e1a50d901b1e0c8a6f7bfa0e71ce00ffecdf5b7
Victor Bondarenko <nirasimha@gmail.com>
2021-12-09 15:12:48 -0500

    Requesting the Days from proxy server

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,6 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props){
-
   const parsedDayList = props.days.map(day => <DayListItem 
     key={day.id} {...day} 
     selected={day.name === props.value} 
