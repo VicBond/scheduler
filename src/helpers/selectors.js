47349ed5b44f37e75a06b4afecb1fa41da497486
Victor Bondarenko <nirasimha@gmail.com>
2021-12-11 11:35:13 -0500

    getInterviewersForDay func updated

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,20 +1,23 @@
 export const getAppointmentsForDay = (state, day) => {
-  const filteredDays = state.days.find(elem => elem.name === day);
+  const filteredDays = state.days.filter(elem => elem.name === day);
 
   let result = [];
 
   if (!filteredDays) {
     return result;
   }
-
-  for (const id in state.appointments) {
-    if (filteredDays.appointments.includes(Number(id))) {
-      result.push(state.appointments[id]);
-    }
+  if (filteredDays.length){
+    result = filteredDays[0].appointments.map(elem => state.appointments[elem]);
   }
+  // for (const id in state.appointments) {
+  //   if (filteredDays.appointments.includes(Number(id))) {
+  //     result.push(state.appointments[id]);
+  //   }
+  // }
 
   return result;
 };
+
 
 export const getInterview = (state, interview) => {
   if (!interview) return null;
@@ -23,4 +26,26 @@ export const getInterview = (state, interview) => {
     student : interview.student,
     interviewer : state.interviewers[interview.interviewer]
   };
+};
+
+
+export const getInterviewersForDay = (state, day) => {
+  const filteredDays = state.days.filter(elem => elem.name === day);
+
+  let result = [];
+
+  if (!filteredDays) {
+    return result;
+  }
+
+  if (filteredDays.length){
+    result = filteredDays[0].interviewers.map(elem => state.interviewers[elem]);
+  }
+  // for (const id in state.interviewers) {
+  //   if (filteredDays.interviewers.includes(Number(id))) {
+  //     result.push(state.interviewers[id]);
+  //   }
+  // }
+
+  return result;
 };
