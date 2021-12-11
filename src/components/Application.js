5cfae24e4dc85dc19447cea7e28cc0817c6d320d
Victor Bondarenko <nirasimha@gmail.com>
2021-12-11 11:36:29 -0500

    interviewers added

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import "./Application.scss";
 import axios from 'axios';
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-import {getAppointmentsForDay, getInterview} from "helpers/selectors";
+import {getAppointmentsForDay, getInterview, getInterviewersForDay} from "helpers/selectors";
 
 
 export default function Application(props) {
@@ -15,6 +15,7 @@ export default function Application(props) {
   });
   
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const dailyInterviewers = getInterviewersForDay(state, state.day);
   const setDay = day => setState({ ...state, day });
   // const setDays = days => setState(prev => ({ ...prev, days }));
 
@@ -37,6 +38,8 @@ export default function Application(props) {
       <Appointment 
       key={appointment.id} 
       {...appointment}
+      interview={interview}
+      interviewers={dailyInterviewers}
       />
     )
   });
