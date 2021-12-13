de04c674ec70501e0c54fc57a0194060cdb1a1bb
Victor Bondarenko <nirasimha@gmail.com>
2021-12-13 18:33:25 -0500

    interviews fixed

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,41 +1,34 @@
 export const getAppointmentsForDay = (state, day) => {
   const filteredDays = state.days.filter(elem => elem.name === day);
 
-  let result = [];
-
-  if (!filteredDays) {
-    return result;
+  if (filteredDays.length > 0) {
+    const obj = filteredDays[0].appointments.map(id => state.appointments[id]);
+    return obj;
+  } else {
+    return filteredDays;
   }
-  if (filteredDays.length){
-    result = filteredDays[0].appointments.map(elem => state.appointments[elem]);
-  }
-
-  return result;
 };
-
 
 export const getInterview = (state, interview) => {
   if (!interview) return null;
-  
-  return {
-    student : interview.student,
-    interviewer : state.interviewers[interview.interviewer]
-  };
-};
 
+  for (const id in state.interviewers) {
+    if (Number(id) === interview.interviewer) {
+      return {
+        student: interview.student,
+        interviewer: state.interviewers[id]
+      };
+    }
+  }
+};
 
 export const getInterviewersForDay = (state, day) => {
   const filteredDays = state.days.filter(elem => elem.name === day);
 
-  let result = [];
-
-  if (!filteredDays) {
-    return result;
+  if (filteredDays.length > 0) {
+    const obj = filteredDays[0].interviewers.map(id => state.interviewers[id]);
+    return obj;
+  } else {
+    return filteredDays;
   }
-
-  if (filteredDays.length){
-    result = filteredDays[0].interviewers.map(elem => state.interviewers[elem]);
-  }
-
-  return result;
 };
