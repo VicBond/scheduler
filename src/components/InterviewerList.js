9e6ff66f13f9dd36ed4517554ea2f3ba098d9162
Victor Bondarenko <nirasimha@gmail.com>
2021-12-14 04:28:49 -0500

    prop-types library added

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "./InterviewerList.scss";
-
+import PropTypes from "prop-types";
 
 
 export default function InterviewerList(props) {
@@ -21,3 +21,7 @@ return(
 </section>
 );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
