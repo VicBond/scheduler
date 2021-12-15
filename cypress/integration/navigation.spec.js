d8483eb66868548d1f042b8be8b63fcd6e9527f1
Victor Bondarenko <nirasimha@gmail.com>
2021-12-15 09:42:08 -0500

    cypress tests for navigation and day selecting with mock data

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,29 @@
 describe("Navigation", () => {
+
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+  });
+
+  it("should find item that contains the text Tuesday", () => {
+    cy.get("li").contains("Tuesday").click();
+  });
+
+  it("should finds the list item, click on it and check it for the correct background colour on Tuesday", () => {
+    cy.contains("li", "Tuesday")
+      .click()
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
+  });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+  
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected")
+  });
+
 });
